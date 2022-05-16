af865666419fd94823115fdd0c89f0c1859fe48b
Aliaksei <sladkiy.aleksey@gmail.com>
2022-05-16 18:50:02 +0300

    fix: rotate text Mon may 16.05.2022 18:50

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -7,64 +7,6 @@ let cont2_3 = document.querySelector('.cont2_3')
 let cont1_4 = document.querySelector('.cont1_4')
 let cont2_4 = document.querySelector('.cont2_4')
 
-//cont1_1.addEventListener('mouseover', colorHover1);
-//function colorHover1 () {
-   // if (!cont2_1.classList.contains('act')) {
-   // cont2_1.style.background = '#DAD8D2'
-//}
-//}
-
-//cont1_1.addEventListener('mouseout', colorAnHover1);
-//function colorAnHover1 () {
-  //  if (!cont2_1.classList.contains('act')) {
- //   cont2_1.style.background = 'grey'
-  //  }
-//}
-
-//cont1_2.addEventListener('mouseover', colorHover2);
-//function colorHover2 () {
-   // if (cont2_2.style.backgroundColor = 'white') {
-  //  cont2_2.style.backgroundColor = '#DAD8D2'
-  //  console.log(5)
-   //}
-//}
-
-//cont1_2.addEventListener('mouseout', colorAnHover2);
-//function colorAnHover2 () {
-  //  if (cont2_2.style.backgroundColor = '#DAD8D2') {
-  //  cont2_2.style.background = 'grey'
- //   } 
-//}
-
-//cont1_3.addEventListener('mouseover', colorHover3);
-//function colorHover3 () {
-  //  if (!cont2_3.classList.contains('act')) {
-   // cont2_3.style.background = '#DAD8D2'
-  //  }
-//}
-
-//cont1_3.addEventListener('mouseout', colorAnHover3);
-//function colorAnHover3 () {
- //   if (!cont2_3.classList.contains('act')) {
-  //  cont2_3.style.background = 'grey'
-  //  }
-//}
-
-
-//cont1_4.addEventListener('mouseover', colorHover4);
-//function colorHover4 () {
-   // if (!cont2_4.classList.contains('act')) {
-  //  cont2_4.style.background = '#DAD8D2'
-   // }
-//}
-
-//cont1_4.addEventListener('mouseout', colorAnHover4);
-//function colorAnHover4 () {
-    //if (!cont2_4.classList.contains('act')) {
-   // cont2_4.style.background = 'grey'
-   // }
-//}
-
 let img = document.querySelector('.img');
 let text = document.querySelector('.text')
 
@@ -84,10 +26,11 @@ function background1 () {
         cont2_3.classList.remove('act')}
     if (cont2_4.classList.contains('act')) {
         cont2_4.classList.remove('act')}
-        text.textContent = "Eat without me again!?";
+        
         text.style.transform = "rotate(360deg)"
         text.style.transition = "transform 1s ease";
         setTimeout(() => {
+          text.textContent = "Eat without me again!?";
            text.style.transform = "none";
          }, 1000) 
 }
@@ -109,10 +52,11 @@ function background2 () {
             cont2_4.classList.remove('act')}
         cont2_2.classList.add("act");
        
-        text.textContent = "Intermission!";
+        
          text.style.transform = "rotate(360deg)"
          text.style.transition = "transform 1s ease";
          setTimeout(() => {
+          text.textContent = "Intermission!";
             text.style.transform = "none";
           }, 1000)         
 }
@@ -135,10 +79,11 @@ function background3 () {
         cont2_1.classList.remove('act')}
     if (cont2_4.classList.contains('act')) {
         cont2_4.classList.remove('act')}
-        text.textContent = "Get in line, ship!";
+        
         text.style.transform = "rotate(360deg)"
         text.style.transition = "transform 1s ease";
         setTimeout(() => {
+          text.textContent = "Get in line, ship!";
            text.style.transform = "none";
          }, 1000) 
 }
@@ -159,10 +104,11 @@ function background4 () {
         cont2_1.classList.remove('act')}
     if (cont2_3.classList.contains('act')) {
         cont2_3.classList.remove('act')}
-        text.textContent = "And did you protect yourself?";
+        
         text.style.transform = "rotate(360deg)"
         text.style.transition = "transform 1s ease";
         setTimeout(() => {
+          text.textContent = "And did you protect yourself?";
            text.style.transform = "none";
          }, 1000) 
 }

bd39b1f9e9f97085020a6da04a9a3033fd2f97bd
Aliaksei <sladkiy.aleksey@gmail.com>
2022-05-16 18:34:55 +0300

    feat: add adaptive

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -23,16 +23,17 @@ let cont2_4 = document.querySelector('.cont2_4')
 
 //cont1_2.addEventListener('mouseover', colorHover2);
 //function colorHover2 () {
- //   if (!cont2_2.classList.contains('act')) {
-   // cont2_2.style.background = '#DAD8D2'
-  //  }
+   // if (cont2_2.style.backgroundColor = 'white') {
+  //  cont2_2.style.backgroundColor = '#DAD8D2'
+  //  console.log(5)
+   //}
 //}
 
 //cont1_2.addEventListener('mouseout', colorAnHover2);
 //function colorAnHover2 () {
-  //  if (!cont2_2.classList.contains('act')) {
+  //  if (cont2_2.style.backgroundColor = '#DAD8D2') {
   //  cont2_2.style.background = 'grey'
-  //  }
+ //   } 
 //}
 
 //cont1_3.addEventListener('mouseover', colorHover3);
@@ -68,12 +69,12 @@ let img = document.querySelector('.img');
 let text = document.querySelector('.text')
 
 cont1_1.addEventListener('click', background1);
-function background1 () {
-    img.setAttribute('src', "assets/img/cat.jpg");
+function background1 () {    
     cont2_1.classList.add("act")
     img.style.transform = "scale(0.5)";
     img.style.transition = "transform 1s ease";
     setTimeout(() => {
+        img.setAttribute('src', "assets/img/cat.jpg");
         img.style.transform = "scale(1)";
     img.style.transition = "transform 1s ease";
       }, 1000)
@@ -83,16 +84,21 @@ function background1 () {
         cont2_3.classList.remove('act')}
     if (cont2_4.classList.contains('act')) {
         cont2_4.classList.remove('act')}
-        text.textContent = "Eat without me again!?"
+        text.textContent = "Eat without me again!?";
+        text.style.transform = "rotate(360deg)"
+        text.style.transition = "transform 1s ease";
+        setTimeout(() => {
+           text.style.transform = "none";
+         }, 1000) 
 }
 
 cont1_2.addEventListener('click', background2);
-function background2 () {
-    img.setAttribute('src', "assets/img/antract.jpg");
+function background2 () {    
     img.style.transform = "scale(0.5)";
     img.style.transition = "transform 1s ease";
     setTimeout(() => {
         img.style.transform = "scale(1)";
+        img.setAttribute('src', "assets/img/antract.jpg");
     img.style.transition = "transform 1s ease";
       }, 1000)
     if (cont2_1.classList.contains('act')) {
@@ -102,17 +108,24 @@ function background2 () {
         if (cont2_4.classList.contains('act')) {
             cont2_4.classList.remove('act')}
         cont2_2.classList.add("act");
-        text.textContent = "Intermission!"
+       
+        text.textContent = "Intermission!";
+         text.style.transform = "rotate(360deg)"
+         text.style.transition = "transform 1s ease";
+         setTimeout(() => {
+            text.style.transform = "none";
+          }, 1000)         
 }
    
 
 cont1_3.addEventListener('click', background3);
-function background3 () {
-    img.setAttribute('src', "assets/img/ship.jpg");
+function background3 () {    
     img.style.transform = "scale(0.5)";
     img.style.transition = "transform 1s ease";
     setTimeout(() => {
+        img.setAttribute('src', "assets/img/ship.jpg");
         img.style.transform = "scale(1)";
+        
     img.style.transition = "transform 1s ease";
       }, 1000)
     cont2_3.classList.add('act')
@@ -122,15 +135,20 @@ function background3 () {
         cont2_1.classList.remove('act')}
     if (cont2_4.classList.contains('act')) {
         cont2_4.classList.remove('act')}
-        text.textContent = "Get in line, ship!"
+        text.textContent = "Get in line, ship!";
+        text.style.transform = "rotate(360deg)"
+        text.style.transition = "transform 1s ease";
+        setTimeout(() => {
+           text.style.transform = "none";
+         }, 1000) 
 }
 
 cont1_4.addEventListener('click', background4);
-function background4 () {
-    img.setAttribute('src', "assets/img/health.jpg");
+function background4 () {    
     img.style.transform = "scale(0.5)";
     img.style.transition = "transform 1s ease";
     setTimeout(() => {
+        img.setAttribute('src', "assets/img/health.jpg");
         img.style.transform = "scale(1)";
     img.style.transition = "transform 1s ease";
       }, 1000)
@@ -141,5 +159,10 @@ function background4 () {
         cont2_1.classList.remove('act')}
     if (cont2_3.classList.contains('act')) {
         cont2_3.classList.remove('act')}
-        text.textContent = "And did you protect yourself?"
+        text.textContent = "And did you protect yourself?";
+        text.style.transform = "rotate(360deg)"
+        text.style.transition = "transform 1s ease";
+        setTimeout(() => {
+           text.style.transform = "none";
+         }, 1000) 
 }
